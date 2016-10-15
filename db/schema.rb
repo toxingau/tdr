c5adb102bc348c51ac45bd3c1d2b5d15d98919d8
dhung133 <dhug133@gmail.com>
2016-10-15 20:42:07 +0700

    show user

--- db/schema.rb
+++ db/schema.rb
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20161002025855) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["location_id"], name: "index_reviews_on_location_id"
+    t.index ["user_id", "created_at"], name: "index_reviews_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
