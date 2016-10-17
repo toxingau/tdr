66ff1d861f88d2323c955645f152ebfed3e5b283
toxingau <toxingau@gmail.com>
2016-10-17 13:03:39 +0700

    finish

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,8 +3,10 @@ class ReviewsController < ApplicationController
 
   def create
     @review = current_user.reviews.build review_params
+    @location = @review.location
     @comment = Comment.new
     if @review.save
+      update_rate @location if @review.rate.present?
       flash[:success] = t :success
       redirect_to @review.location
     else
@@ -17,12 +19,18 @@ class ReviewsController < ApplicationController
     @review = current_user.reviews.find_by_id params[:id]
     @location = @review.location
     @review.destroy
+    update_rate @location if @review.rate.present?
     flash[:danger] = t :destroyed
     redirect_to root_url
   end
 
   private
   def review_params
-    params.require(:review).permit :content, :location_id
+    params.require(:review).permit :content, :location_id, :rate
+  end
+
+  def update_rate location
+    rating = location.reviews.where.not(rate: nil).average(:rate)
+    location.update_attributes rating: rating    
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -8,4 +8,6 @@ class Review < ApplicationRecord
   validates :location_id, presence: true
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 255}
+  validates :rate, numericality: 
+    {greater_than_or_equal_to: 0, less_than_or_equal_to: 10}
 end
