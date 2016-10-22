6f75ff9e6b6017e3769858e819b589b0f0e1f355
toxingau <toxingau@gmail.com>
2016-10-22 12:00:22 +0700

    finish fix bug 1

--- app/controllers/rater_controller.rb
+++ app/controllers/rater_controller.rb
@@ -2,12 +2,18 @@ class RaterController < ApplicationController
 
   def create
     if user_signed_in?
-      obj = params[:klass].classify.constantize.find(params[:id])
+      obj = params[:klass].classify.constantize.find_by_id params[:id]
       obj.rate params[:score].to_f, current_user, params[:dimension]
+      rate = if obj.rates("service").size == 1
+        obj.rates("service").first.stars
+      else
+        obj.average("service").avg
+      end
+      obj.update_attributes rating: rate.round(2)
 
-      render :json => true
+      render json: true
     else
-      render :json => false
+      render json: false
     end
   end
 end
