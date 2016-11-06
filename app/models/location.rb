d4966ed1d68f02816d18695a94cd5eebafe53d45
dhung133 <dhug133@gmail.com>
2016-11-06 20:25:19 +0700

    fix-bug

--- app/models/location.rb
+++ app/models/location.rb
@@ -14,7 +14,7 @@ class Location < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
-  validates :name, length: {maximum: 150}, presence: true, uniqueness: true
+  validates :name, length: {maximum: 150}, presence: true
   validates :category_id, presence: false
   validates :rating, presence: true,format: {with: /\A\d+(?:.\d{0,2})?\z/},
     numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
